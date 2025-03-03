13d620770d16fa16671ff3d565e6a5c1fe640856
Sophie Wu <swu.lee02@gmail.com>
2025-03-03 14:03:46 -0800

    Remove loading audio

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,8 @@ if uploaded_file is not None:
     upload_status = st.success(f"File uploaded: {uploaded_file.name}")
     processing_status = st.info(f"{uploaded_file.name} is now being converted...")
 
-    audio_caption = st.caption("The audio player below will play sounds to indicate the status of your document conversion.")
-    loading_audio = st.audio("static/loading.wav", format="audio/wav", loop=True, autoplay=True)
+    # audio_caption = st.caption("The audio player below will play sounds to indicate the status of your document conversion.")
+    # loading_audio = st.audio("static/loading.wav", format="audio/wav", loop=True, autoplay=True)
 
     try:    
         # Send file to Flask API
@@ -42,7 +42,7 @@ if uploaded_file is not None:
         response = requests.post(FLASK_API_URL, files=files)
         response_json = response.json()
 
-        loading_audio.empty()
+        # loading_audio.empty()
 
         if response.status_code == 200:
             success_audio_placeholder = st.empty()
@@ -52,7 +52,7 @@ if uploaded_file is not None:
 
             upload_status.empty()
             processing_status.empty()
-            audio_caption.empty()
+            # audio_caption.empty()
 
             st.success("Document converted successfully! Your converted document is displayed below.")
             st.markdown(response_json["md"]) 
@@ -64,12 +64,12 @@ if uploaded_file is not None:
 
             upload_status.empty()
             processing_status.empty()
-            audio_caption.empty()
+            # audio_caption.empty()
             
             st.error(f"There was an error converting your document. Error: {response}")
 
     except Exception as e:
-        loading_audio.empty()
+        # loading_audio.empty()
 
         error_audio_placeholder = st.empty()
         error_audio_placeholder.audio("static/error.wav", format="audio/wav", loop=False, autoplay=True)
@@ -78,7 +78,7 @@ if uploaded_file is not None:
 
         upload_status.empty()
         processing_status.empty()
-        audio_caption.empty()
+        # audio_caption.empty()
         
         st.error(f"There was an error processing your document. Error: {e}")
 
